f7ae2bd13575bfcbf40baf0df19aa5599415fce3
eufanzky <eufran@DESKTOP-7SJ3DVO.localdomain>
2022-10-30 14:49:33 -0400

    red lines when winning added

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const startButton = document.querySelector('#start-button');
 const historyButton = document.querySelector('#history-button');
 
 //lines wehbn winning
-const lines = document.querySelectorAll('.line')
-let linesArray = [...lines]
+const lines = document.querySelectorAll('.line');
+let linesArray = [...lines];
 
 //divs
 const divs = document.querySelectorAll('.game-container__box');
-let divsArray = [...divs]
+let divsArray = [...divs];
 
 //switch to know the turn
 let sw = 0;
@@ -40,12 +40,12 @@ const verifyGameState = () => {
     let game = divsArray.map(item => {
         return item.textContent;
     });
-    // console.log("rows x " + verifyRows(game, 'x'));
-    // console.log("rows o " + verifyRows(game, 'o'));
-    // console.log("columns x " + verifyColumns(game, 'x'));
-    // console.log("columns o " + verifyColumns(game, 'o'));
-    // console.log("diagonals x " + verifyDiagonals(game, 'x'));
-    // console.log("diagonals o " + verifyDiagonals(game, 'o'));
+    verifyRows(game,'x');
+    verifyRows(game,'o');
+    verifyColumns(game, 'x');
+    verifyColumns(game, 'o');
+    verifyDiagonals(game, 'x');
+    verifyDiagonals(game, 'o');
 }
 
 const verifyRows = (game, value) => {
@@ -56,31 +56,65 @@ const verifyRows = (game, value) => {
     let gameRow3 = [...game].splice(6,3);
 
 
-    if(JSON.stringify(gameRow1) === JSON.stringify(row) 
-    || JSON.stringify(gameRow2) === JSON.stringify(row) 
-    || JSON.stringify(gameRow3) === JSON.stringify(row)) {
-        return true;
-    } else {
-        return false;
+    if(JSON.stringify(gameRow1) === JSON.stringify(row))  {
+        const rowRedLine = document.querySelector('.row1-line');
+        rowRedLine.classList.toggle('element--display-none');
+        finishGame();
+    } else if (JSON.stringify(gameRow2) === JSON.stringify(row) ) {
+        const rowRedLine = document.querySelector('.row2-line');
+        rowRedLine.classList.toggle('element--display-none');
+        finishGame();
+
+    } else if (JSON.stringify(gameRow3) === JSON.stringify(row)) {
+        const rowRedLine = document.querySelector('.row3-line');
+        rowRedLine.classList.toggle('element--display-none');
+        finishGame();
+
     }
 }
 const verifyColumns = (game, value) => {
-    if(game[0] === value && game[3] === value && game[6] === value
-    ||game[1] === value && game[4] === value && game[7] === value
-    ||game[2] === value && game[5] === value && game[8] === value
-    ) {
-        return true;
-    } else {
-        return false;
+    if(game[0] === value && game[3] === value && game[6] === value) {
+        const columnLine = document.querySelector('.column1-line')
+        columnLine.classList.toggle('element--display-none');
+        finishGame();
+
+    } else if (game[1] === value && game[4] === value && game[7] === value) {
+        const columnLine = document.querySelector('.column2-line');
+        columnLine.classList.toggle('element--display-none');
+        finishGame();
+
+    } else if(game[2] === value && game[5] === value && game[8] === value) {
+        const columnLine = document.querySelector('.column3-line');
+        columnLine.classList.toggle('element--display-none');
+        finishGame();
+
     }
+    
 }
 const verifyDiagonals = (game, value) => {
-    if(game[0] === value && game[4] === value && game[8] === value
-    ||game[2] === value && game[4] === value && game[6] === value
-    ) {
-        return true;
-    } else {
-        return false;
+    if(game[0] === value && game[4] === value && game[8] === value) {
+        const diagonalLine = document.querySelector('.diagonal1-line');
+        diagonalLine.classList.toggle('element--display-none');
+        finishGame();
+
+    } else if (game[2] === value && game[4] === value && game[6] === value) {
+        const diagonalLine = document.querySelector('.diagonal2-line');
+        diagonalLine.classList.toggle('element--display-none');
+        finishGame();
+
     }
 }
 
+
+const finishGame = (winner) => {
+    divsArray.forEach(div => {
+        div.removeEventListener('click', draw);
+    });
+
+    if(winner === '') {
+        console.log('a');
+    } else {
+        const winnerElement = document.querySelector('.winner');
+        winnerElement.textContent = `Winner! ${winner}`;
+    }
+}
